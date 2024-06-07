f9425a68b356a344983fb3b167c6927c2eb39182
cloudzxv <145342681+cloudzxv@users.noreply.github.com>
2024-06-07 11:41:23 +0300

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 async function logMovies() {
     const response = await fetch("https://api.openf1.org/v1/drivers");
     const drivers = await response.json();
-    const driver = drivers[0]
-    document.body.innerHTML += `<li><a href="#">Item ${driver.full_name}</a></li>`
-  }
-  
+    const driversLess = drivers.slice(0, 10)
+    for (const driver of driversLess) {
+      document.body.innerHTML += `<div>
+          <h1>Driver Number ${driver.driver_number}</h1>
+          <h2>${driver.full_name}</h2>
+          <img src="${driver.headshot_url}" alt="${driver.full_name}">
+          <h3>Team Name: ${driver.team_name}</h3>
+      </div>`
+    }
+}
+
+
   logMovies()
